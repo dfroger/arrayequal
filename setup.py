594f3ab45ee735f344892745990c05b606873894
David Froger <david.froger@inria.fr>
2015-01-13 11:38:43 +0100

    version 0.0.4

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name = 'arrayequal',
-    version = '0.0.3',
+    version = '0.0.4',
     description = 'test for numpy array equality with unittest support',
     author= 'David Froger',
     author_email = 'david.froger@inria.fr',
     url = '',
     packages = ['arrayequal',],
     install_requires = 'numpy',
-    provides = ['arrayequal (0.0.3)'],
+    provides = ['arrayequal (0.0.4)'],
 )
