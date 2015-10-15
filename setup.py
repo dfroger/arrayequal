ee5192572eeec06b998c7b1bf6a0b4e767752be9
David Froger <david.froger@inria.fr>
2015-10-15 16:38:33 +0200

    Bump version number to 0.1.0

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name = 'arrayequal',
-    version = '0.0.4',
+    version = '0.1.0',
     description = 'test for numpy array equality with unittest support',
     author= 'David Froger',
     author_email = 'david.froger@inria.fr',
     url = '',
     packages = ['arrayequal',],
-    install_requires = ['numpy', 'matplotlib', 'Jinja2', 'nose']
-    provides = ['arrayequal (0.0.4)'],
+    install_requires = ['numpy',],
+    provides = ['arrayequal (0.1.0)'],
 )
